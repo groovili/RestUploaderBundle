1025144f68f398b1ca5392134ebf4d47a107adf7
groovili <maxim.ivanov@panenco.com>
2017-12-27 17:50:10 +0200

    TODOS modified

---
+++ Form/DataTransformer/RestFileTransformer.php
@@ -0,0 +1,13 @@
+<?php
+    /**
+     * Created by groovili
+     * https://github.com/groovili
+     */
+    
+    namespace Groovili\RestUploaderBundle\Form\DataTransformer;
+    
+    
+    class RestFileTransformer
+    {
+        
+    }
---
+++ Form/RestFileType.php
@@ -0,0 +1,13 @@
+<?php
+    /**
+     * Created by groovili
+     * https://github.com/groovili
+     */
+    
+    namespace Groovili\RestUploaderBundle\Form;
+    
+    
+    class RestFileType
+    {
+        
+    }
