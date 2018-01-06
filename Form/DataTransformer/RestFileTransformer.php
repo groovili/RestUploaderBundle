72c9485f0ad450288ac40675e4d11849f53aff42
groovili <m.ivanov.work@gmail.com>
2018-01-06 18:26:18 +0200

    Dont upload file if its not valid, existing entites handle

--- Form/DataTransformer/RestFileTransformer.php
+++ Form/DataTransformer/RestFileTransformer.php
@@ -1,114 +1,118 @@
 <?php
+/**
+ *
+ *  * This file is part of the RestUploaderBundle package.
+ *  * (c) groovili
+ *  * For the full copyright and license information, please view the LICENSE
+ *  * file that was distributed with this source code.
+ *
+ */
+declare(strict_types=1);
+
+namespace Groovili\RestUploaderBundle\Form\DataTransformer;
+
+use Doctrine\ORM\EntityManagerInterface;
+use function dump;
+use Groovili\RestUploaderBundle\Entity\File;
+use Groovili\RestUploaderBundle\Service\FileManager;
+use function is_array;
+use function is_numeric;
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+/**
+ * Class RestFileTransformer
+ *
+ * @package Groovili\RestUploaderBundle\Form\DataTransformer
+ */
+class RestFileTransformer implements DataTransformerInterface
+{
     /**
-     *
-     *  * This file is part of the RestUploaderBundle package.
-     *  * (c) groovili
-     *  * For the full copyright and license information, please view the LICENSE
-     *  * file that was distributed with this source code.
-     *
+     * @var \Groovili\RestUploaderBundle\Service\FileManager
      */
-    declare(strict_types = 1);
-    
-    namespace Groovili\RestUploaderBundle\Form\DataTransformer;
-    
-    use Doctrine\ORM\EntityManagerInterface;
-    use Groovili\RestUploaderBundle\Entity\File;
-    use Groovili\RestUploaderBundle\Service\FileManager;
-    use Groovili\RestUploaderBundle\Service\FileValidator;
-    use Symfony\Component\Form\DataTransformerInterface;
-    use Symfony\Component\Form\Exception\TransformationFailedException;
-    use Symfony\Component\HttpFoundation\File\UploadedFile;
-    
+    protected $fileManager;
+
     /**
-     * Class RestFileTransformer
-     *
-     * @package Groovili\RestUploaderBundle\Form\DataTransformer
+     * @var array
      */
-    class RestFileTransformer implements DataTransformerInterface
+    protected $options;
+
+    /**
+     * @var \Doctrine\ORM\EntityManagerInterface
+     */
+    protected $em;
+
+    /**
+     * RestFileTransformer constructor.
+     * @param FileManager $fileManager
+     * @param array $options
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(
+        FileManager $fileManager,
+        array $options,
+        EntityManagerInterface $entityManager
+    ) {
+        $this->fileManager = $fileManager;
+        $this->options = $options;
+        $this->em = $entityManager;
+    }
+
+    /**
+     * @param $data
+     * @return File|null|object
+     */
+    private function search4FileEntity($data)
     {
-        /**
-         * @var \Groovili\RestUploaderBundle\Service\FileManager
-         */
-        protected $fileManager;
-        
-        /**
-         * @var \Groovili\RestUploaderBundle\Service\FileValidator
-         */
-        protected $fileValidator;
-        
-        /**
-         * @var array
-         */
-        protected $options;
-        
-        /**
-         * @var \Doctrine\ORM\EntityManagerInterface
-         */
-        protected $em;
-        
-        /**
-         * @var array
-         */
-        protected static $emptyTransform = null;
-        
-        /**
-         * RestFileTransformer constructor.
-         *
-         * @param \Groovili\RestUploaderBundle\Service\FileManager $fileManager
-         * @param \Groovili\RestUploaderBundle\Service\FileValidator $fileValidator
-         * @param array $options
-         */
-        public function __construct(
-          FileManager $fileManager,
-          array $options,
-          EntityManagerInterface $entityManager
-        ) {
-            $this->fileManager = $fileManager;
-            $this->options = $options;
-            $this->em = $entityManager;
+        if (false === $data || null === $data || !is_array($data)) {
+            return $data;
         }
-        
-        /**
-         * @param mixed $file
-         *
-         * @return array|null
-         */
-        public function transform($file)
-        {
-            if (!$file) {
-                return self::$emptyTransform;
-            }
-            
+
+        if (isset($data['id']) && is_numeric($data['id'])) {
             $fileEntity = $this->em->getRepository('RestUploaderBundle:File')
-                                   ->findOneBy([
-                                     'name' => $file->getClientOriginalName(),
-                                   ]);
-            
+                ->findOneBy([
+                    'id' => $data['id'],
+                ]);
+
             if (null === $fileEntity) {
-                throw new TransformationFailedException(sprintf('A file with name "%s" does not exist!',
-                  $file->getClientOriginalName()));
+                throw new TransformationFailedException(sprintf('A file with id "%s" does not exist!',
+                    $data['id']));
             }
-            
+
             return $fileEntity;
         }
-        
-        /**
-         * @param mixed $data
-         *
-         * @return mixed
-         */
-        public function reverseTransform($file)
-        {
-            if (!$file instanceof UploadedFile) {
-                return self::$emptyTransform;
-            }
-            
+
+
+        return $data;
+    }
+
+    /**
+     * @param mixed $data
+     * @return File|mixed|null|object
+     */
+    public function transform($data)
+    {
+        return $this->search4FileEntity($data);
+    }
+
+    /**
+     * @param mixed $data
+     * @return File|mixed|null|object
+     * @throws \Exception
+     */
+    public function reverseTransform($data)
+    {
+        if ($data instanceof UploadedFile) {
             $private = false;
-            
+
             if (true === $this->options['private']) {
                 $private = true;
             }
-            
-            return $this->fileManager->upload($file, $private);
+
+            return $this->fileManager->upload($data, $private);
         }
+
+        return $this->search4FileEntity($data);
     }
+}
